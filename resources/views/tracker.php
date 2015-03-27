d9a584292c5d6c0bccc5ea3f729f19511abf9c75
Jenny Swift <cheezyspaghetti@gmail.com>
2015-03-27 03:48:04 +0000

    make journal nicer

--- resources/views/tracker.php
+++ resources/views/tracker.php
@@ -52,17 +52,17 @@
 		
 	</div> <!-- end entries tab -->
 
-	<div ng-show="tab.journal">
-		<h1>journal!</h1>
+	<div ng-show="tab.journal" id="journal">
 
 		<!-- <div ng-show="!journal_entry.edit" ng-bind-html="journal_entry.text"></div> -->
 		<div class="btn-toolbar" data-role="editor-toolbar" data-target="#wysiwyg">
 			<a data-edit="bold" class="fa fa-bold"></a>
 		</div>
-		<div ng-bind-html="journal_entry.text" id="journal-entry" class="wysiwyg"></div>
+		<div ng-bind-html="journal_entry.text" id="journal-entry" class="wysiwyg margin-bottom"></div>
 
 		<!-- <button ng-click="journal_entry.edit = true">edit</button> -->
-		<button ng-click="insertOrUpdateJournalEntry()">save entry</button>
+		<button ng-click="insertOrUpdateJournalEntry()" class="btn">save entry</button>
+
 	</div>
 
 	
