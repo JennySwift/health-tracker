d6d7b82709f7eab5fef5f4cc48e213f0f4666a0d
Jenny Swift <cheezyspaghetti@gmail.com>
2015-03-29 06:17:56 +0000

    fix bug-calories for foods were incorrect

--- app/Http/Controllers/InsertController.php
+++ app/Http/Controllers/InsertController.php
@@ -84,10 +84,8 @@ class InsertController extends Controller {
 	public function food () {
 		include(app_path() . '/inc/functions.php');
 		$name = json_decode(file_get_contents('php://input'), true)["name"];
-		DB::table('foods')->insert([
-			'name' => $name,
-			'user_id' => Auth::user()->id
-		]);
+		insertFood($name);
+
 		return getAllFoodsWithUnits();
 	}
 
--- app/inc/functions.php
+++ app/inc/functions.php
@@ -145,32 +145,38 @@ function getAllFoodsWithUnits () {
 		$food_id = $food['id'];
 		$food_name = $food['name'];
 
+	    $food = array(
+			"id" => $food_id,
+			"name" => $food_name
+	    );
+
 		$rows = DB::table('calories')
 			->join('foods', 'food_id', '=', 'foods.id')
 			->join('food_units', 'calories.unit_id', '=', 'food_units.id')
 			->where('food_id', $food_id)
 			->select('food_units.name', 'food_units.id', 'calories', 'default_unit')
 			->get();
-	   
+	   Debugbar::info('rows for ' . $food_name . ' ', $rows);
 
 		$units = array();
 		foreach ($rows as $row) {
 			$unit_name = $row->name;
 			$unit_id = $row->id;
 			$calories = $row->calories;
-			$default_unit = $row->default_unit;			
+			$default_unit = $row->default_unit;
 
 			if ($default_unit === 1) {
 				$default_unit = true;
 				$default_unit_id = $unit_id;
 				$default_unit_name = $unit_name;
 				$default_unit_calories = $calories;
+
+				$food['default_unit_id'] = $default_unit_id;
+				$food['default_unit_name'] = $default_unit_name;
+				$food['default_unit_calories'] = $default_unit_calories;
 			}
 			else {
 				$default_unit = false;
-				// $default_unit_id = '';
-				// $default_unit_name = '';
-				// $default_unit_calories = '';
 			}
 
 			$units[] = array(
@@ -180,32 +186,12 @@ function getAllFoodsWithUnits () {
 				"default_unit" => $default_unit
 			);
 		}
-	    
-	    $food = array(
-			"id" => $food_id,
-			"name" => $food_name
-			// "default_unit_id" => $default_unit_id,
-			// "default_unit_name" => $default_unit_name,
-			// "default_unit_calories" => $default_unit_calories
-	    );
-
-	    if (isset($default_unit_id)) {
-	    	$food['default_unit_id'] = $default_unit_id;
-	    }
-	    if (isset($default_unit_id)) {
-	    	$food['default_unit_name'] = $default_unit_name;
-	    }
-	    if (isset($default_unit_id)) {
-	    	$food['default_unit_calories'] = $default_unit_calories;
-	    }
 
 	    $all_foods_with_units[] = array(
 	    	"food" => $food,
 	    	"units" => $units
 	    );
 	}
-
-	// Debugbar::info('all_foods_with_units', $all_foods_with_units);
     
 	return $all_foods_with_units;
 }
@@ -485,6 +471,13 @@ function getExercises () {
 // =================================insert=================================
 // ========================================================================
 // ========================================================================
+
+function insertFood ($name) {
+	DB::table('foods')->insert([
+		'name' => $name,
+		'user_id' => Auth::user()->id
+	]);
+}
 
 function insertNewExerciseTag ($name) {
 	DB::table('exercise_tags')
