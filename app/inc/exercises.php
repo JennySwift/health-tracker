534108a5e942985c8661ac5b5e6b36b83e790879
Jenny Swift <cheezyspaghetti@gmail.com>
2015-04-02 01:47:13 +0000

    open exercise popup by clicking any of the columns

--- app/inc/exercises.php
+++ app/inc/exercises.php
@@ -20,13 +20,13 @@
 						<th>x</th>
 					</tr>
 					<tr ng-repeat="exercise in exercises" class="hover">
-						<td>{{exercise.name}}</td>
-						<td>{{exercise.description}}</td>
-						<td>{{exercise.step_number}}</td>
-						<td>{{exercise.series_name}}</td>
+						<td ng-click="showExercisePopup(exercise)" class="pointer">{{exercise.name}}</td>
+						<td ng-click="showExercisePopup(exercise)" class="pointer">{{exercise.description}}</td>
+						<td ng-click="showExercisePopup(exercise)" class="pointer">{{exercise.step_number}}</td>
+						<td ng-click="showExercisePopup(exercise)" class="pointer">{{exercise.series_name}}</td>
 						<td ng-click="showExercisePopup(exercise)" class="pointer">{{exercise.default_exercise_unit_name}}</td>
-						<td>{{exercise.default_quantity}}</td>
-						<td>
+						<td ng-click="showExercisePopup(exercise)" class="pointer">{{exercise.default_quantity}}</td>
+						<td ng-click="showExercisePopup(exercise)" class="pointer">
 							<span ng-repeat="tag in exercise.tags" class="badge">{{tag.name}}</span>
 						</td>
 						<td><i ng-click="deleteExercise(exercise.id)" class="delete-item fa fa-times"></i></td>
