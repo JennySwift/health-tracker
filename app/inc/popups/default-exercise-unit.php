55c6258f5d67f17155a5d4294545bd7ed991766f
Jenny Swift <cheezyspaghetti@gmail.com>
2015-04-02 01:34:29 +0000

    keep making exercise popup look nicer

--- app/inc/popups/default-exercise-unit.php
+++ app/inc/popups/default-exercise-unit.php
@@ -29,15 +29,15 @@
 				<input ng-keyup="updateDefaultExerciseQuantity($event.keyCode)" type="number" placeholder="enter quantity" id="default-unit-quantity">
 			</div>
 			
-			<div>
-				<h5 class="center">tags</h5>
-				<div id="default-exercise-unit-popup-checkboxes">
-					<label ng-repeat="tag in exercise_tags">
-						{{tag.name}}
-						<input checklist-model="selected.exercise.tags" checklist-value="tag" type="checkbox">
-					</label>
-				</div>
-			</div>
+		</div>
+
+		<h5 class="center">tags</h5>
+
+		<div id="default-exercise-unit-popup-checkboxes" class="flex">
+			<label ng-repeat="tag in exercise_tags">
+				{{tag.name}}
+				<input checklist-model="selected.exercise.tags" checklist-value="tag" type="checkbox">
+			</label>
 		</div>
 
 		<button ng-click="insertTagsInExercise()">done</button>
