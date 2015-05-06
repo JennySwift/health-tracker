a6ec317fd824d42b559cfbb9df4ada11fc3ef087
Jenny Swift <cheezyspaghetti@gmail.com>
2015-05-06 00:58:17 +0000

    Start moving each migration into its own file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,34 +24,6 @@ class CreateUsersTable extends Migration {
 			$table->timestamps();
 		});
 
-		Schema::create('weight', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->date('date');
-			$table->decimal('weight', 10, 2);
-			$table->integer('user_id')->unsigned(); //foreign key
-
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-
-		Schema::create('exercises', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->string('name');
-			$table->integer('user_id')->unsigned(); //foreign key
-			$table->decimal('step_number', 10, 2)->nullable();
-			$table->integer('series_id')->nullable()->unsigned(); //foreign key
-			$table->decimal('default_quantity', 10, 2)->nullable();
-			$table->string('description')->nullable();
-			$table->integer('default_exercise_unit_id')->nullable()->unsigned(); //foreign key
-
-			$table->foreign('default_exercise_unit_id')->references('id')->on('exercise_units');
-			$table->foreign('series_id')->references('id')->on('exercise_series');
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-
 		Schema::create('exercise_entries', function(Blueprint $table)
 		{
 			$table->increments('id');
@@ -67,52 +39,7 @@ class CreateUsersTable extends Migration {
 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 		});
 
-		Schema::create('foods', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->integer('user_id')->unsigned(); //foreign key
-			$table->string('name');
-
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-
-		Schema::create('food_units', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->string('name');
-			$table->integer('user_id')->unsigned(); //foreign key
-
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-
-		Schema::create('recipes', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->string('name');
-			$table->integer('user_id')->unsigned(); //foreign key
-
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-
 		//These are the more tables with more foreign keys
-
-		Schema::create('calories', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->integer('food_id')->unsigned(); //foreign key
-			$table->integer('unit_id')->unsigned(); //foreign key
-			$table->decimal('calories', 10, 2)->nullable();
-			$table->boolean('default_unit')->nullable();
-			$table->integer('user_id')->unsigned(); //foreign key
-
-			$table->foreign('food_id')->references('id')->on('foods')->onDelete('cascade');
-			$table->foreign('unit_id')->references('id')->on('food_units')->onDelete('cascade');	
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
 
 		Schema::create('food_entries', function(Blueprint $table)
 		{
@@ -131,23 +58,6 @@ class CreateUsersTable extends Migration {
 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 		});
 
-		Schema::create('food_recipe', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-			$table->integer('recipe_id')->unsigned(); //foreign key
-			$table->integer('food_id')->unsigned(); //foreign key
-			$table->integer('unit_id')->unsigned(); //foreign key
-			$table->integer('user_id')->unsigned(); //foreign key
-			$table->decimal('quantity', 10, 2);
-			$table->string('description')->nullable();
-
-			$table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
-			$table->foreign('food_id')->references('id')->on('foods')->onDelete('cascade');
-			$table->foreign('unit_id')->references('id')->on('food_units')->onDelete('cascade');
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-
 		DB::statement('SET FOREIGN_KEY_CHECKS=1');
 	}
 
@@ -160,15 +70,9 @@ class CreateUsersTable extends Migration {
 	{
 		DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
-		Schema::drop('weight');
 		Schema::drop('exercise_entries');
-		Schema::drop('exercises');
-		Schema::drop('calories');
 		Schema::drop('food_entries');
-		Schema::drop('food_recipe');
 		Schema::drop('foods');
-		Schema::drop('food_units');
-		Schema::drop('recipes');	
 		Schema::drop('users');
 
 		DB::statement('SET FOREIGN_KEY_CHECKS=1');
