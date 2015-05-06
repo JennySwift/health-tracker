845c78e8b6c2b3c60eaf06347ee966c049fbd979
Jenny Swift <cheezyspaghetti@gmail.com>
2015-05-06 02:10:50 +0000

    Add missing create table statement for exercises migration

---
+++ database/migrations/2015_05_06_020933_create_exercises_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExercisesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('exercises', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->string('name');
+			$table->integer('user_id')->unsigned(); //foreign key
+			$table->decimal('step_number', 10, 2)->nullable();
+			$table->integer('series_id')->nullable()->unsigned(); //foreign key
+			$table->decimal('default_quantity', 10, 2)->nullable();
+			$table->string('description')->nullable();
+			$table->integer('default_exercise_unit_id')->nullable()->unsigned(); //foreign key
+
+			$table->foreign('default_exercise_unit_id')->references('id')->on('exercise_units');
+			$table->foreign('series_id')->references('id')->on('exercise_series');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('exercises');
+	}
+
+}
