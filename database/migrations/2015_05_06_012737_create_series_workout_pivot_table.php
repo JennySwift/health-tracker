1f362eea16399752bd7c9b1f5f4bc317341d1c1f
Jenny Swift <cheezyspaghetti@gmail.com>
2015-05-06 01:47:58 +0000

    Rename series_workout_table migration to series_workout_pivot_table

---
+++ database/migrations/2015_05_06_012737_create_series_workout_pivot_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesWorkoutPivotTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$table->increments('id');
+		$table->timestamps();
+		$table->integer('workout_id')->unsigned(); //foreign key
+		$table->integer('series_id')->unsigned(); //foreign key
+		$table->integer('user_id')->unsigned(); //foreign key
+
+		$table->foreign('workout_id')->references('id')->on('workouts')->onDelete('cascade');
+		$table->foreign('series_id')->references('id')->on('exercise_series')->onDelete('cascade');
+		$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
