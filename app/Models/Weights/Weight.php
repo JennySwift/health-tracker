5def94e09ea75e6b47751e1b729344e580595966
Valentin Prugnaud <foxted@Valentins-MacBook-Air.local>
2015-05-18 02:36:12 -0700

    Repository refactor example with weights and SOLID principles example

--- app/Models/Weights/Weight.php
+++ app/Models/Weights/Weight.php
@@ -8,59 +8,9 @@ class Weight extends Model {
 	/**
 	 * Define relationships
 	 */
-
 	public function user()
 	{
 		return $this->belongsTo('App\User');
 	}
-
-	/**
-	 * select
-	 */
-	
-	public static function getWeight($date)
-	{
-		$weight = static
-			::where('date', $date)
-			->where('user_id', Auth::user()->id)
-			->pluck('weight');
-
-		if (!$weight) {
-			$weight = 0;
-		}
-		return $weight;
-	}
-	
-	/**
-	 * insert
-	 */
-	
-	public static function insertWeight($date, $weight)
-	{
-		static
-			::insert([
-				'date' => $date,
-				'weight' => $weight,
-				'user_id' => Auth::user()->id
-			]);
-	}
-
-	/**
-	 * update
-	 */
-	
-	public static function updateWeight($date, $weight)
-	{
-		static
-			::where('date', $date)
-			->where('user_id', Auth::user()->id)
-			->update([
-				'weight' => $weight
-			]);
-	}
-
-	/**
-	 * delete
-	 */
 
 }
