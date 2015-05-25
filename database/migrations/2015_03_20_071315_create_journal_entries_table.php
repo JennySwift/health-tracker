b6235187085b45cf3dca750d6001bf93762581f2
Jenny Swift <cheezyspaghetti@gmail.com>
2015-05-25 14:58:22 +1000

    Fix error message when running migrations and seeders after indexing columns

--- database/migrations/2015_03_20_071315_create_journal_entries_table.php
+++ database/migrations/2015_03_20_071315_create_journal_entries_table.php
@@ -17,10 +17,19 @@ class CreateJournalEntriesTable extends Migration {
 			$table->increments('id')->index();
 			$table->integer('user_id')->unsigned()->index();
 			$table->date('date')->index();
-			$table->text('text')->index();
+			$table->text('text');
 			$table->timestamps();
 			
 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+			/**
+			 * @VP:
+			 * When I tried adding ->index() to my 'text' column, and ran my migrations and seeders, I got the following error:
+			 * [Illuminate\Database\QueryException]
+  			 * SQLSTATE[42000]: Syntax error or access violation: 1170 BLOB/TEXT column 'text' used in key spec
+  			 * ification without a key length (SQL: alter table `journal_entries` add index journal_entries_tex
+  			 * t_index(`text`))
+			 */
 		});
 	}
 
