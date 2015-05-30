d9d1bcc5220644d750434c73ae9fda5d019404b1
Jenny Swift <cheezyspaghetti@gmail.com>
2015-05-30 22:07:43 +1000

    Delete unused file

--- app/Valentin.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/**
-	 * I have some models for pivot tables. I'm not sure if they should exist. I remember you said something about Laravel taking care of the pivot tables and so I didn't need a (controller? model?) for my pivot tables. Not sure where all the code in my pivot table models should go if that's the case. I've added comments at the top of those files so you will know which ones they are.
-	 */
-	
-	/**
-	 * Please refactor a few of each of the following queries: select, insert, update and delete. (So I get the idea of how to use Eloquent for each type of query.)
-	 */
-?>
