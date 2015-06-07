1a994d9035f45c42040655776487ec2f1ed34681
Jenny Swift <cheezyspaghetti@gmail.com>
2015-06-07 18:34:37 +1000

    Show project popup for when user is payer

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -76,13 +76,15 @@
                 <th></th>
             </tr>
             <tr ng-repeat="project in projects.payer">
-                <td>[[project.payee.name]] <img ng-src="[[project.payee.gravatar]]"></td>
-                <td>[[project.description]]</td>
-                <td>$[[project.rate_per_hour]]</td>
-                <td>
+                <td ng-click="showProjectPopup(project)" class="pointer">[[project.payee.name]]
+                    <img ng-src="[[project.payee.gravatar]]">
+                </td>
+                <td ng-click="showProjectPopup(project)" class="pointer">[[project.description]]</td>
+                <td ng-click="showProjectPopup(project)" class="pointer">$[[project.rate_per_hour]]</td>
+                <td ng-click="showProjectPopup(project)" class="pointer">
                     [[project.total_time_user_formatted.hours]]:[[project.total_time_user_formatted.minutes]]:[[project.total_time_user_formatted.seconds]]
                 </td>
-                <td>[[project.formatted_price]]</td>
+                <td ng-click="showProjectPopup(project)" class="pointer">[[project.formatted_price]]</td>
                 {{--<td>--}}
                 {{--<span ng-if="!project.paid" class="label label-danger">unpaid</span>--}}
                 {{--<span ng-if="project.paid" class="label label-success">paid</span>--}}
