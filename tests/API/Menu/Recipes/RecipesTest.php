9b04b9d43781d35c9e154318bc4f61a604558d11
Jenny Swift <cheezyspaghetti@gmail.com>
2015-11-07 18:47:00 +1100

    Work on test for inserting quick recipe

--- tests/API/Menu/Recipes/RecipesTest.php
+++ tests/API/Menu/Recipes/RecipesTest.php
@@ -67,42 +67,66 @@ class RecipesTest extends TestCase {
      * @test
      * @return void
      */
-//    public function it_can_insert_a_quick_recipe()
-//    {
-//        $this->logInUser();
-//
-//        $data = [
-//            'check_for_similar_names' => true,
-//            'name' => 'super recipe',
-//            'ingredients' => [
-//                [
+    public function it_can_insert_a_quick_recipe()
+    {
+        $this->logInUser();
+
+        $data = [
+            'check_for_similar_names' => true,
+            'name' => 'super recipe',
+            'ingredients' => [
+                [
+                    'description' => 'chopped',
+                    'food' => 'apple',
+                    'quantity' => 1,
+                    'unit' => 'small'
+                ],
+                [
 //                    'description' => 'chopped',
-//                    'food' => 'apple',
-//                    'quantity' => 1,
-//                    'unit' => 'small'
-//                ],
-//                [
-////                    'description' => 'chopped',
-//                    'food' => 'bananas',
-//                    'quantity' => 2,
-//                    'unit' => 'large'
-//                ]
-//            ],
-//            'steps' => ['Blend', 'Eat']
-//        ];
-//
-//        $response = $this->call('POST', '/api/quickRecipes', $data);
-////        dd($response);
-//        $content = json_decode($response->getContent(), true)['data'];
-////        dd($content);
-//
-//        $this->assertArrayHasKey('id', $content);
-//        $this->assertArrayHasKey('name', $content);
-//
-//        $this->assertEquals('kangaroo', $content['name']);
-//
-//        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
-//    }
+                    'food' => 'bananas',
+                    'quantity' => 2,
+                    'unit' => 'large'
+                ]
+            ],
+            'steps' => ['Blend', 'Eat']
+        ];
+
+        $response = $this->call('POST', '/api/quickRecipes', $data);
+//        dd($response);
+        $content = json_decode($response->getContent(), true)['data'];
+//        dd($content);
+
+        $this->assertArrayHasKey('id', $content);
+        $this->assertArrayHasKey('name', $content);
+        $this->assertArrayHasKey('steps', $content);
+        $this->assertArrayHasKey('tag_ids', $content);
+        $this->assertArrayHasKey('ingredients', $content);
+        $this->assertArrayHasKey('tags', $content);
+
+        $this->assertEquals('super recipe', $content['name']);
+        $this->assertEquals('Blend', $content['steps'][0]['text']);
+        $this->assertEquals(1, $content['steps'][0]['step']);
+        $this->assertEquals('Eat', $content['steps'][1]['text']);
+        $this->assertEquals(2, $content['steps'][1]['step']);
+
+//        $this->assertEquals(7, $content['ingredients'][0]['food_id']);
+        $this->assertEquals('apple', $content['ingredients'][0]['name']);
+        $this->assertEquals('small', $content['ingredients'][0]['unit_name']);
+        $this->assertEquals(3, $content['ingredients'][0]['unit_id']);
+        $this->assertEquals('1.00', $content['ingredients'][0]['quantity']);
+        $this->assertEquals('chopped', $content['ingredients'][0]['description']);
+        $this->assertEquals([], $content['ingredients'][0]['units']);
+
+//        $this->assertEquals(8, $content['ingredients'][0]['food_id']);
+        $this->assertEquals('bananas', $content['ingredients'][1]['name']);
+        $this->assertEquals('large', $content['ingredients'][1]['unit_name']);
+        $this->assertEquals(5, $content['ingredients'][1]['unit_id']);
+        $this->assertEquals('2.00', $content['ingredients'][1]['quantity']);
+        $this->assertEquals('', $content['ingredients'][1]['description']);
+        $this->assertEquals([], $content['ingredients'][1]['units']);
+
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+    }
 
     /**
      * @test
