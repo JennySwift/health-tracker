101a8a1467d886d1af5428950299a87c144a80f6
Jenny Swift <cheezyspaghetti@gmail.com>
2015-11-30 12:10:47 +1100

    Add daytime sleeps to seeder

--- database/seeds/SleepSeeder.php
+++ database/seeds/SleepSeeder.php
@@ -10,6 +10,7 @@ class SleepSeeder extends Seeder {
 
     private $user;
     private $faker;
+    private $date;
 
     public function run()
 	{
@@ -26,19 +27,45 @@ class SleepSeeder extends Seeder {
              * Create entries for the last 50 days.
              */
             foreach (range(0, 49) as $index) {
-                $today = Carbon::today();
-                $date = $today->subDays($index);
-
-                $entry = new Sleep([
-                    'start' => $date->hour(21)->format('Y-m-d H:i:s'),
-                    'finish' => $date->addDays(1)->hour(8)->minute($this->faker->randomElement([0,15,30,45]))->format('Y-m-d H:i:s')
-                ]);
-
-                $entry->user()->associate($this->user);
-                $entry->save();
+                $this->createDaySleep($index);
+                $this->createNightSleep($index);
             }
         }
 
 	}
+
+    /**
+     * Create a night's sleep
+     */
+    private function createNightSleep($index)
+    {
+        $today = Carbon::today();
+        $this->date = $today->subDays($index);
+
+        $entry = new Sleep([
+            'start' => $this->date->hour(21)->format('Y-m-d H:i:s'),
+            'finish' => $this->date->addDays(1)->hour(8)->minute($this->faker->randomElement([0,15,30,45]))->format('Y-m-d H:i:s')
+        ]);
+
+        $entry->user()->associate($this->user);
+        $entry->save();
+    }
+
+    /**
+     * Create a day's sleep
+     */
+    private function createDaySleep($index)
+    {
+        $today = Carbon::today();
+        $this->date = $today->subDays($index);
+
+        $entry = new Sleep([
+            'start' => $this->date->hour(16)->format('Y-m-d H:i:s'),
+            'finish' => $this->date->hour(17)->minute($this->faker->randomElement([0,15,30,45]))->format('Y-m-d H:i:s')
+        ]);
+
+        $entry->user()->associate($this->user);
+        $entry->save();
+    }
 
 }
