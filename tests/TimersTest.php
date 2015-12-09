6025539e91cc7b77ffb5e796614eaea312408bb6
Jenny Swift <cheezyspaghetti@gmail.com>
2015-12-10 09:05:30 +1100

    Write test for checking if there is a timer in progress

--- tests/TimersTest.php
+++ tests/TimersTest.php
@@ -201,4 +201,38 @@ class TimersTest extends TestCase {
 
         DB::rollBack();
     }
+
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_checks_for_timer_in_progress()
+    {
+        DB::beginTransaction();
+        $this->logInUser();
+
+        $timer = [
+            'start' => '2015-12-01 21:00:00',
+            'activity_id' => Activity::where('name', 'work')->first()->id
+        ];
+
+        $response = $this->call('POST', '/api/timers', $timer);
+        $content = json_decode($response->getContent(), true);
+
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+
+        $response = $this->call('GET', '/api/timers/checkForTimerInProgress');
+        $content = json_decode($response->getContent(), true);
+
+        $this->assertArrayHasKey('id', $content);
+        $this->assertArrayHasKey('start', $content);
+        $this->assertArrayHasKey('startDate', $content);
+        $this->assertArrayHasKey('activity', $content);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        DB::rollBack();
+    }
+
 }
