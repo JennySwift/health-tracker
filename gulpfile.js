90e10e4ab282cf80ca63c430e677dc04526d9b82
Jenny Swift <cheezyspaghetti@gmail.com>
2015-12-12 11:44:07 +1100

    Use versioning for styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('style.scss');
 
+    mix.version("css/style.css");
+
     mix.scripts([
         'plugins/*.js',
         'app.js',
@@ -12,13 +14,15 @@ elixir(function(mix) {
         'filters/**/*.js'
     ], 'public/js/all.js');
 
-    mix.scripts([
-        'jasmine/spec/*.js'
-    ], 'public/js/jasmine/specs.js');
+    //mix.version("js/all.js");
 
-    mix.scripts([
-        'jasmine/src/*.js'
-    ], 'public/js/jasmine/src.js');
+    //mix.scripts([
+    //    'jasmine/spec/*.js'
+    //], 'public/js/jasmine/specs.js');
+    //
+    //mix.scripts([
+    //    'jasmine/src/*.js'
+    //], 'public/js/jasmine/src.js');
 
     mix.stylesIn('resources/assets/css', 'public/css/plugins.css');
 });
