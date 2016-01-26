33d4e6912ab1dbe98db74a2c6ccaa383cd53f756
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-27 08:30:28 +1100

    Clean series page

--- resources/views/pages/exercises/series-exercises.blade.php
+++ resources/views/pages/exercises/series-exercises.blade.php
@@ -3,25 +3,19 @@
         <tr>
             <th>Step</th>
             <th>Name</th>
-            <th>Description</th>
             <th>Priority</th>
-            <th>Default</th>
             <th>Target</th>
-            <th>Program</th>
             <th>Days ago</th>
             <th>Add</th>
         </tr>
         <tr ng-repeat="exercise in selected.exercise_series.exercises.data | filter:{'priority': seriesPriorityFilter}">
             <td ng-click="showExercisePopup(exercise)">[[exercise.stepNumber]]</td>
             <td ng-click="showExercisePopup(exercise)">[[exercise.name]]</td>
-            <td ng-click="showExercisePopup(exercise)">[[exercise.description]]</td>
             <td ng-click="showExercisePopup(exercise)">[[exercise.priority]]</td>
-            <td ng-click="showExercisePopup(exercise)">[[exercise.defaultQuantity]]</td>
             <td ng-click="showExercisePopup(exercise)">[[exercise.target]]</td>
-            <td ng-click="showExercisePopup(exercise)">[[exercise.program.name]]</td>
             <td ng-click="showExercisePopup(exercise)">[[exercise.lastDone]]</td>
             <td>
-                <button ng-click="insertExerciseSet(exercise)" class="btn btn-default btn-xs">Add set</button>
+                <button ng-click="insertExerciseSet(exercise)" class="btn btn-default btn-xs"><i class="fa fa-plus"></i> [[exercise.defaultQuantity]] [[exercise.defaultUnit.name]]</button>
             </td>
         </tr>
     </table>
--- resources/views/pages/exercises/series.blade.php
+++ resources/views/pages/exercises/series.blade.php
@@ -38,18 +38,26 @@
             <div>
                 <table id="series-table" class="table table-bordered">
                     <tr>
-                        <th>series</th>
-                        <th>days ago</th>
-                        <th>history</th>
-                        <th>priority</th>
-                        <th>edit</th>
+                        <th>Series</th>
+                        <th>Days ago</th>
+                        <th>Priority</th>
                     </tr>
                     <tr ng-repeat="series in exercise_series | orderBy: 'lastDone' | filter:{'priority': seriesPriorityFilter}" ng-class="{'selected': series.id === selected.exercise_series.id}">
-                        <td ng-click="getExercisesInSeries(series)" class="name">[[series.name]]</td>
+                        <td>
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    [[series.name]] <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a ng-click="getExercisesInSeries(series)" href="#">Exercises</a></li>
+                                    <li><a ng-click="getExerciseSeriesHistory(series)" href="#">History</a></li>
+                                    <li><a ng-click="showExerciseSeriesPopup(series)" href="#">Edit</a></li>
+                                </ul>
+                            </div>
+                        </td>
+
                         <td ng-click="getExercisesInSeries(series)" class="name">[[series.lastDone]]</td>
-                        <td><button ng-click="getExerciseSeriesHistory(series)">history</button></td>
                         <td>[[series.priority]]</td>
-                        <td><button ng-click="showExerciseSeriesPopup(series)" class="btn-xs">edit</button></td>
                     </tr>
                 </table>
             </div>
