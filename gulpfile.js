3c496d2311cd5853c3aa8429728198620c2578e6
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-27 11:58:39 +1100

    Setup for Vue

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,24 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('style.scss');
 
-    //mix.version("css/style.css");
-
     mix.scripts([
         'plugins/*.js',
+        'config.js',
+
         'app.js',
         'controllers/**/*.js',
         'factories/**/*.js',
         'directives/**/*.js',
-        'filters/**/*.js'
+        'filters/**/*.js',
+
+        //'helpers.js',
+        'repositories/**/*.js',
+        'components/**/*.js',
+        //'app.js',
+        //'routes.js'
     ], 'public/js/all.js');
 
-    //mix.version("js/all.js");
-
     mix.version(["css/style.css", "js/all.js"]);
-
-    //mix.scripts([
-    //    'jasmine/spec/*.js'
-    //], 'public/js/jasmine/specs.js');
-    //
-    //mix.scripts([
-    //    'jasmine/src/*.js'
-    //], 'public/js/jasmine/src.js');
-
     mix.stylesIn('resources/assets/css', 'public/css/plugins.css');
 });
+
