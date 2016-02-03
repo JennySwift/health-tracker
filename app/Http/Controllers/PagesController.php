f12010fbc08bc39fbd5b95e9cac85fb8a219badb
Jenny Swift <cheezyspaghetti@gmail.com>
2016-02-03 15:01:12 +1100

    Clean up PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,22 +2,11 @@
 
 use App\Http\Requests;
 use App\Http\Transformers\ExerciseEntryTransformer;
-use App\Http\Transformers\JournalTransformer;
-use App\Http\Transformers\SeriesTransformer;
-use App\Models\Journal\Journal;
-use App\Models\Tags\Tag;
 use App\Repositories\CaloriesRepository;
 use App\Repositories\ExerciseEntriesRepository;
-use App\Repositories\ExerciseSeriesRepository;
-use App\Repositories\ExercisesRepository;
-use App\Repositories\ExerciseTagsRepository;
-use App\Repositories\FoodsRepository;
 use App\Repositories\MenuEntriesRepository;
-use App\Repositories\RecipesRepository;
-use App\Repositories\RecipeTagsRepository;
 use App\Repositories\UnitsRepository;
 use App\Repositories\WeightsRepository;
-use App\Repositories\WorkoutsRepository;
 use Auth;
 use Carbon\Carbon;
 use JavaScript;
@@ -28,14 +17,6 @@ use JavaScript;
  */
 class PagesController extends Controller
 {
-    /**
-     * @var ExercisesRepository
-     */
-    private $exercisesRepository;
-    /**
-     * @var FoodsRepository
-     */
-    private $foodsRepository;
     /**
      * @var UnitsRepository
      */
@@ -48,72 +29,27 @@ class PagesController extends Controller
      * @var MenuEntriesRepository
      */
     private $menuEntriesRepository;
-    /**
-     * @var RecipesRepository
-     */
-    private $recipesRepository;
-    /**
-     * @var WorkoutsRepository
-     */
-    private $workoutsRepository;
+
     /**
      * @var CaloriesRepository
      */
     private $caloriesRepository;
-    /**
-     * @var ExerciseSeriesRepository
-     */
-    private $exerciseSeriesRepository;
-    /**
-     * @var ExerciseTagsRepository
-     */
-    private $exerciseTagsRepository;
-    /**
-     * @var RecipeTagsRepository
-     */
-    private $recipeTagsRepository;
 
     /**
      * Create a new controller instance.
      *
-     * @param ExercisesRepository $exercisesRepository
-     * @param FoodsRepository $foodsRepository
      * @param UnitsRepository $unitsRepository
      * @param ExerciseEntriesRepository $exerciseEntriesRepository
      * @param MenuEntriesRepository $menuEntriesRepository
-     * @param RecipesRepository $recipesRepository
-     * @param WorkoutsRepository $workoutsRepository
      * @param CaloriesRepository $caloriesRepository
-     * @param ExerciseSeriesRepository $exerciseSeriesRepository
-     * @param ExerciseTagsRepository $exerciseTagsRepository
-     * @param RecipeTagsRepository $recipeTagsRepository
      */
-    public function __construct(
-        ExercisesRepository $exercisesRepository,
-        FoodsRepository $foodsRepository,
-        UnitsRepository $unitsRepository,
-        ExerciseEntriesRepository $exerciseEntriesRepository,
-        MenuEntriesRepository $menuEntriesRepository,
-        RecipesRepository $recipesRepository,
-        WorkoutsRepository $workoutsRepository,
-        CaloriesRepository $caloriesRepository,
-        ExerciseSeriesRepository $exerciseSeriesRepository,
-        ExerciseTagsRepository $exerciseTagsRepository,
-        RecipeTagsRepository $recipeTagsRepository
-    )
+    public function __construct(UnitsRepository $unitsRepository, CaloriesRepository $caloriesRepository, ExerciseEntriesRepository $exerciseEntriesRepository, MenuEntriesRepository $menuEntriesRepository)
     {
         $this->middleware('auth');
-        $this->exercisesRepository = $exercisesRepository;
-        $this->foodsRepository = $foodsRepository;
         $this->unitsRepository = $unitsRepository;
+        $this->caloriesRepository = $caloriesRepository;
         $this->exerciseEntriesRepository = $exerciseEntriesRepository;
         $this->menuEntriesRepository = $menuEntriesRepository;
-        $this->recipesRepository = $recipesRepository;
-        $this->workoutsRepository = $workoutsRepository;
-        $this->caloriesRepository = $caloriesRepository;
-        $this->exerciseSeriesRepository = $exerciseSeriesRepository;
-        $this->exerciseTagsRepository = $exerciseTagsRepository;
-        $this->recipeTagsRepository = $recipeTagsRepository;
     }
 
     /**
@@ -125,10 +61,7 @@ class PagesController extends Controller
     {
         $date = Carbon::today()->format('Y-m-d');
 
-//        dd($this->menuEntriesRepository->getEntriesForTheDay($date));
-
         JavaScript::put([
-//            "exerciseEntries" => $this->exerciseEntriesRepository->getEntriesForTheDay($date),
             "exerciseEntries" => transform(
                 createCollection(
                     $this->exerciseEntriesRepository->getEntriesForTheDay($date),
@@ -146,161 +79,6 @@ class PagesController extends Controller
         ]);
 
         return view('pages.entries.entries-page');
-    }
-
-
-    /**
-     *
-     * @param ExercisesRepository $exercisesRepository
-     * @return \Illuminate\View\View
-     */
-//    public function exercises()
-//    {
-//        JavaScript::put([
-//            /**
-//             * @VP:
-//             * If I instead did:
-//             * 'exercises' => $this->getExercises()
-//             * it didn't work. Why wouldn't it let me call it 'exercises?'
-//             * The same thing happened when I tried to use 'tags' instead of 'exercise_tags.'
-//             */
-//            'all_exercises' => $this->exercisesRepository->getExercises(),
-//            'series' => $this->exerciseSeriesRepository->getExerciseSeries(),
-//            'workouts' => $this->workoutsRepository->getWorkouts(),
-//            'units' => $this->unitsRepository->getExerciseUnits(),
-//            'exercise_tags' => Tag::forCurrentUser()
-//                ->where('for', 'exercise')
-//                ->orderBy('name', 'asc')
-//                ->get()
-//
-//        ]);
-//
-//        return view('pages.exercises.exercises-page');
-//    }
-
-    /**
-     * Exercise series page
-     * @return \Illuminate\View\View
-     */
-//    public function series()
-//    {
-//        JavaScript::put([
-//            'series' => transform(createCollection($this->exerciseSeriesRepository->getExerciseSeries(), new SeriesTransformer))['data'],
-//            'workouts' => $this->workoutsRepository->getWorkouts(),
-//            "exerciseEntries" => transform(
-//                createCollection(
-//                    $this->exerciseEntriesRepository->getEntriesForTheDay(Carbon::today()->format('Y-m-d')),
-//                    new ExerciseEntryTransformer
-//                )
-//            )['data'],
-//            "exerciseUnits" => $this->unitsRepository->getExerciseUnits(),
-//        ]);
-//
-//        return view('pages.exercises.series-page');
-//    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-    public function foods()
-    {
-        JavaScript::put([
-            'foods' => $this->foodsRepository->getFoods(),
-        ]);
-
-        return view('pages.menu.foods.foods-page');
-    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-//    public function recipes()
-//    {
-//        JavaScript::put([
-//            'foods_with_units' => $this->foodsRepository->getFoods(),
-//            'recipes' => $this->recipesRepository->filterRecipes('', []),
-//            'recipe_tags' => $this->recipeTagsRepository->getRecipeTags()
-//        ]);
-//
-//        return view('pages.menu.recipes.recipes-page');
-//    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-    public function foodUnits()
-    {
-        JavaScript::put([
-            'units' => $this->unitsRepository->getFoodUnits()
-        ]);
-
-        return view('pages.menu.foods.food-units');
-    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-//    public function exerciseUnits()
-//    {
-//        JavaScript::put([
-//            'units' => $this->unitsRepository->getExerciseUnits()
-//        ]);
-//
-//        return view('pages.exercises.exercise-units');
-//    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-//    public function journal()
-//    {
-//        $date = Carbon::today()->format('Y-m-d');
-//
-//        $entry = Journal::forCurrentUser()->where('date', $date)->first();
-//        if ($entry) {
-//            $entry = transform(createItem($entry, new JournalTransformer))['data'];
-//        }
-//        else {
-//            $entry = [];
-//        }
-//
-//        JavaScript::put([
-//            'entry' => $entry
-//        ]);
-//
-//        return view('pages.journal.journal-page');
-//    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-//    public function timers()
-//    {
-//        return view('pages.timers.timers-page');
-//    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-//    public function timerActivities()
-//    {
-//        return view('pages.timers.activities-page');
-//    }
-
-    /**
-     *
-     * @return \Illuminate\View\View
-     */
-    public function timerGraphs()
-    {
-        return view('pages.timers.graphs');
     }
 
     /**
