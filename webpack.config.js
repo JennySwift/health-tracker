8f1a24570c26842b616ce328425bef78cc897197
Jenny Swift <cheezyspaghetti@gmail.com>
2016-08-24 21:10:52 +1000

    Setup for vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,29 @@ module.exports = {
     plugins: [
         // new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
     ],
-    devtool: 'source-map'
+    devtool: 'source-map',
+
+
+    //For vue-loader
+    module: {
+        // `loaders` is an array of loaders to use.
+        // here we are only configuring vue-loader
+        loaders: [
+            {
+                test: /\.vue$/, // a regex for matching all files that end in `.vue`
+                loader: 'vue'   // loader to use for matched files
+            },
+            {
+                // use babel-loader for *.js files
+                test: /\.js$/,
+                loader: 'babel',
+                // important: exclude files in node_modules
+                // otherwise it's going to be really slow!
+                exclude: /node_modules/
+            }
+        ]
+    },
+    babel: {
+        presets: ['es2015']
+    }
 };
